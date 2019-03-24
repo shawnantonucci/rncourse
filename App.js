13d2050f66c17ad99e20f53c5a570c26581168af
shawn <shawnantonucci2018@gmail.com>
2019-03-24 10:39:59 -0700

    wired up input and display a list out

--- App.js
+++ App.js
@@ -3,25 +3,48 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default class App extends Component {
   state = {
-    placeName: ""
+    placeName: "",
+    places: []
   };
 
-  placeNameChangeHandler = val => {
-    this.setState({ placeName: val });
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
   };
 
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <Text key={i}>{place}</Text>
+    ));
     return (
       <View style={styles.container}>
         <View style={styles.inputContainer}>
           <TextInput
-            style={styles.placeInput}
             placeholder="An awesome place"
             value={this.state.placeName}
-            onChangeText={this.placeNameChangeHandler}
+            onChangeText={this.placeNameChangedHandler}
+            style={styles.placeInput}
           />
-          <Button style={styles.placeButton} title="Add" />
+          <Button
+            title="Add"
+            style={styles.placeButton}
+            onPress={this.placeSubmitHandler}
+          />
         </View>
+        <View>{placesOutput}</View>
       </View>
     );
   }
@@ -30,13 +53,11 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    justifyContent: "center",
+    padding: 26,
+    backgroundColor: "#fff",
     alignItems: "center",
-    backgroundColor: "#F5FCFF",
     justifyContent: "flex-start"
   },
-
   inputContainer: {
     // flex: 1,
     width: "100%",
@@ -44,11 +65,9 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center"
   },
-
   placeInput: {
     width: "70%"
   },
-
   placeButton: {
     width: "30%"
   }
