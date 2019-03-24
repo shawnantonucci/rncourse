479c51a6b44d235fbe6557b3d57b13c3eb8a6c3e
shawn <shawnantonucci2018@gmail.com>
2019-03-24 11:06:40 -0700

    added listitem view

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
+import ListItem from './src/components/ListItem/ListItem'
+
 export default class App extends Component {
   state = {
     placeName: "",
@@ -27,7 +29,7 @@ export default class App extends Component {
 
   render() {
     const placesOutput = this.state.places.map((place, i) => (
-      <Text key={i}>{place}</Text>
+      <ListItem key={i} placeName={place} />
     ));
     return (
       <View style={styles.container}>
@@ -44,7 +46,7 @@ export default class App extends Component {
             onPress={this.placeSubmitHandler}
           />
         </View>
-        <View>{placesOutput}</View>
+        <View style={styles.listContainer}>{placesOutput}</View>
       </View>
     );
   }
@@ -70,5 +72,8 @@ const styles = StyleSheet.create({
   },
   placeButton: {
     width: "30%"
+  },
+  listContainer: {
+    width: "100%"
   }
 });
