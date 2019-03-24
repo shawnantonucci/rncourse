a3d3abd20b11327e8c9c11e783fda6a40cd34288
shawn <shawnantonucci2018@gmail.com>
2019-03-24 11:17:47 -0700

    extracted placeInput

--- App.js
+++ App.js
@@ -2,27 +2,17 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 import ListItem from './src/components/ListItem/ListItem'
+import PlaceInput from './src/components/PlaceInput/PlaceInput'
 
 export default class App extends Component {
   state = {
-    placeName: "",
     places: []
   };
 
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  };
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
-
+  placeAddedHandler = (placeName) => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(prevState.placeName)
+        places: prevState.places.concat(placeName)
       };
     });
   };
@@ -33,19 +23,7 @@ export default class App extends Component {
     ));
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="An awesome place"
-            value={this.state.placeName}
-            onChangeText={this.placeNameChangedHandler}
-            style={styles.placeInput}
-          />
-          <Button
-            title="Add"
-            style={styles.placeButton}
-            onPress={this.placeSubmitHandler}
-          />
-        </View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <View style={styles.listContainer}>{placesOutput}</View>
       </View>
     );
@@ -60,19 +38,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start"
   },
-  inputContainer: {
-    // flex: 1,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  placeInput: {
-    width: "70%"
-  },
-  placeButton: {
-    width: "30%"
-  },
+
   listContainer: {
     width: "100%"
   }
