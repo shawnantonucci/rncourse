f101e84afb7c372711def8410af3610ef9dbc457
shawn <shawnantonucci2018@gmail.com>
2019-03-24 21:55:44 -0700

    added a sidedrawer

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import AuthScreen from "./src/screens/Auth/Auth";
 import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
 import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
 import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+import SideDrawer from './src/screens/SideDrawer/SideDrawer';
 import configureStore from "./src/store/configureStore";
 
 const store = configureStore();
@@ -43,6 +44,11 @@ Navigation.registerComponent(
   () => PlaceDetailScreen,
   store,
   Provider
+);
+
+Navigation.registerComponent(
+  "awesome-places.SideDrawer",
+  () => SideDrawer
 );
 
 // Start a App
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { connect } from "react-redux";
 
-import PlaceInput from './../../components/PlaceInput/PlaceInput';
-import { addPlace } from '../../store/actions/index'
+import PlaceInput from "./../../components/PlaceInput/PlaceInput";
+import { addPlace } from "../../store/actions/index";
 
 class SharePlaceScreen extends Component {
-    placeAddedHandler = (placeName) => {
-        this.props.onAddPlace(placeName);
-    }
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
 
-    render () {
-        return (
-            <View>
-                <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-            </View>
-        );
+  onNavigatorEvent = event => {
+    if (event.type === "NavBarButtonPress") {
+      if (event.id === "sideDrawerToggle") {
+        this.props.navigator.toggleDrawer({
+          side: "left"
+        });
+      }
     }
+  };
+
+  placeAddedHandler = placeName => {
+    this.props.onAddPlace(placeName);
+  };
+
+  render() {
+    return (
+      <View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+      </View>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onAddPlace: (placeName) => dispatch(addPlace(placeName))
-    }
-}
+  return {
+    onAddPlace: placeName => dispatch(addPlace(placeName))
+  };
+};
 
-export default connect(null, mapDispatchToProps)(SharePlaceScreen);
+export default connect(
+  null,
+  mapDispatchToProps
+)(SharePlaceScreen);
