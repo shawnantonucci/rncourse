d28d971f3695746b1e6eacae9a5d8856675a9abd
shawn <shawnantonucci2018@gmail.com>
2019-03-24 18:41:19 -0700

    working on navigation and auth

--- android/app/src/main/java/com/rncourse/MainActivity.java
+++ android/app/src/main/java/com/rncourse/MainActivity.java
@@ -1,15 +1,16 @@
 package com.rncourse;
 
 import com.facebook.react.ReactActivity;
-
-public class MainActivity extends ReactActivity {
+import com.reactnativenavigation.controllers.SplashActivity;
+ 
+public class MainActivity extends SplashActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "rncourse";
-    }
+    // @Override
+    // protected String getMainComponentName() {
+    //     return "rncourse";
+    // }
 }
