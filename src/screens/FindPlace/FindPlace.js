68c5db98eaec9057f23dc66ea8f182bb1071fc5c
shawn <shawnantonucci2018@gmail.com>
2019-03-24 20:28:29 -0700

    setup adding a place and listing them

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 
-class FindPlace extends Component {
+import PlaceList from '../../components/PlaceList/PlaceList';
+
+class FindPlaceScreen extends Component {
     render () {
         return (
             <View>
-                <Text>On FindPlaceScreen</Text>
+                <PlaceList places={this.props.places} />
             </View>
         );
     }
 }
 
-export default FindPlace;
+const mapStateToProps = state => {
+    return {
+        places: state.places.places
+    };
+};
+
+export default connect(mapStateToProps)(FindPlaceScreen);
--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actionTypes';
+import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName) => {
     return {
@@ -12,16 +12,3 @@ export const deletePlace = () => {
         type: DELETE_PLACE
     };
 };
-
-export const selectPlace = (key) => {
-    return {
-        type: SELECT_PLACE,
-        placeKey: key
-    };
-};
-
-export const deselectPlace = () => {
-    return {
-        type: DESELECT_PLACE
-    };
-}
