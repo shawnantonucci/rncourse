80be0296a3b4f9df90ea7ded5d5d9f38d6e97166
shawn <shawnantonucci2018@gmail.com>
2019-03-27 10:27:37 -0700

    working map api

--- android/app/src/main/java/com/rncourse/MainApplication.java
+++ android/app/src/main/java/com/rncourse/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.NavigationApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
  
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +26,8 @@ public class MainApplication extends NavigationApplication {
     // No need to add RnnPackage and MainReactPackage
     return Arrays.<ReactPackage>asList(
       new MainReactPackage(),
-      new VectorIconsPackage()
+      new VectorIconsPackage(),
+      new MapsPackage()
     );
   }
  
