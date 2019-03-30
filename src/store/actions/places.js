1c1575135845b73cfb4393b3c0ca3fdaf8572e6d
shawn <shawnantonucci2018@gmail.com>
2019-03-29 18:54:22 -0700

    linked firebase storage and sending an image there

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,17 +1,31 @@
-import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
+import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
 
 export const addPlace = (placeName, location, image) => {
-    return {
-        type: ADD_PLACE,
-        placeName: placeName,
-        location: location,
-        image: image
-    };
+  return dispatch => {
+    fetch(
+      "https://us-central1-rn-course-1553656358152.cloudfunctions.net/storeImage",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          image: image.base64
+        })
+      }
+    )
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        const placeData = {
+          name: placeName,
+          location: location,
+          image: parsedRes.imageUrl
+        };
+      });
+  };
 };
 
-export const deletePlace = (key) => {
-    return {
-        type: DELETE_PLACE,
-        placeKey: key
-    };
+export const deletePlace = key => {
+  return {
+    type: DELETE_PLACE,
+    placeKey: key
+  };
 };
