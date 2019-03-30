7647d5afe4ef7a2b62accfdd81ecb7323a975cc3
shawn <shawnantonucci2018@gmail.com>
2019-03-29 19:00:55 -0700

    added image link and data to database

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,31 +1,37 @@
-import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
+import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-  return dispatch => {
-    fetch(
-      "https://us-central1-rn-course-1553656358152.cloudfunctions.net/storeImage",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          image: image.base64
+    return dispatch => {
+        fetch("https://us-central1-rn-course-1553656358152.cloudfunctions.net/storeImage", {
+            method: "POST",
+            body: JSON.stringify({
+                image: image.base64
+            })
         })
-      }
-    )
-      .catch(err => console.log(err))
-      .then(res => res.json())
-      .then(parsedRes => {
-        const placeData = {
-          name: placeName,
-          location: location,
-          image: parsedRes.imageUrl
-        };
-      });
-  };
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            const placeData = {
+                name: placeName,
+                location: location,
+                image: parsedRes.imageUrl
+            };
+            return fetch("https://rn-course-1553656358152.firebaseio.com//places.json", {
+                method: "POST",
+                body: JSON.stringify(placeData)
+            })
+        })  
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes);
+        });
+    };
 };
 
-export const deletePlace = key => {
-  return {
-    type: DELETE_PLACE,
-    placeKey: key
-  };
+export const deletePlace = (key) => {
+    return {
+        type: DELETE_PLACE,
+        placeKey: key
+    };
 };
