3f5864a5246ee04dd49d5a33f7e91ede7225c604
shawn <shawnantonucci2018@gmail.com>
2019-03-31 23:43:59 -0700

    add navigate to findplaces after adding one

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,5 +1,11 @@
-import { SET_PLACES, REMOVE_PLACE } from "./actionTypes";
+import { SET_PLACES, REMOVE_PLACE, PLACE_ADDED, START_ADD_PLACE } from "./actionTypes";
 import { uiStartLoading, uiStopLoading, authGetToken } from "./index";
+
+export const startAddPlace = () => {
+  return {
+    type: START_ADD_PLACE
+  }
+}
 
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
@@ -49,6 +55,7 @@ export const addPlace = (placeName, location, image) => {
       .then(parsedRes => {
         console.log(parsedRes);
         dispatch(uiStopLoading());
+        dispatch(placeAdded());
       })
       .catch(err => {
         console.log(err);
@@ -57,6 +64,12 @@ export const addPlace = (placeName, location, image) => {
       });
   };
 };
+
+export const placeAdded = () => {
+  return {
+    type: PLACE_ADDED
+  }
+}
 
 export const getPlaces = () => {
   return dispatch => {
