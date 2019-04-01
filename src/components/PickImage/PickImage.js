d7cf253f66d63cbb75bce01e7a49696d342fa8b1
shawn <shawnantonucci2018@gmail.com>
2019-03-31 23:09:16 -0700

    restricted image size uploaded

--- src/components/PickImage/PickImage.js
+++ src/components/PickImage/PickImage.js
@@ -8,7 +8,7 @@ class PickImage extends Component {
   }
 
   pickImageHandler = () => {
-    ImagePicker.showImagePicker({title: "Pick an Image"}, res => {
+    ImagePicker.showImagePicker({title: "Pick an Image", maxWidth: 800, maxHeight: 600}, res => {
       if (res.didCancel) {
         console.log("User cancelled!");
       } else if (res.error) {
